c0271ee10975772fc43f8153ba8d33d28954c2b1
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-08-11 00:21:34 +0200

    Added some code to simple_webapp.py and tested how dict keyerror works

--- R10/simple_webapp.py
+++ R10/simple_webapp.py
@@ -21,3 +21,24 @@ def page2():
 @app.route('/page3')
 def page3():
     return 'To jest strona 3.'
+
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'Teraz jesteś zalogowany.'
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'Teraz jesteś wylogowany.'
+
+@app.route('/status')
+def check_status() -> str:
+    if 'logged_in' in session:
+        return 'W tej chwili jesteś zalogowany.'
+    return 'NIE jesteś zalogowany.'
+
+app.secret_key = 'NigdyNieZgadnieszMojegoTajnegoKlucza'
+
+if __name__ == '__main__':
+    app.run(debug=True)
